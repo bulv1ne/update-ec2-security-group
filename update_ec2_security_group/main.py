8d07892f715526d431bb8e0d59bb22f2aa39de02
Niels Lemmens <1936122+bulv1ne@users.noreply.github.com>
2025-12-22 10:38:46 +0100

    Changes

--- update_ec2_security_group/main.py
+++ update_ec2_security_group/main.py
@@ -1,45 +1,78 @@
-from pprint import pprint
+from contextlib import contextmanager
 
 import boto3
+import inquirer
+import requests
 from botocore.exceptions import ClientError
+from mypy_boto3_ec2 import EC2Client
 
 
 def main():
     client = boto3.client("ec2")
-    for sg in client.describe_security_groups()["SecurityGroups"]:
-        if sg["GroupName"] == "sleipnir-server":
-            print(f"Updating security group: {sg['GroupName']}")
-            try:
-                client.authorize_security_group_ingress(
-                    GroupId=sg["GroupId"],
-                    IpPermissions=[
-                        {
-                            "IpProtocol": "tcp",
-                            "FromPort": 22,
-                            "ToPort": 22,
-                            "IpRanges": [{"CidrIp": "0.0.0.0/0"}],
-                        },
-                    ],
-                )
-            except ClientError as e:
-                if e.response['Error']['Code'] == 'InvalidPermission.Duplicate':
-                    print("Ingress rule already exists.")
-                else:
-                    raise
-            try:
-                input("Press any key to continue...")
-            finally:
-                client.revoke_security_group_ingress(
-                    GroupId=sg["GroupId"],
-                    IpPermissions=[
-                        {
-                            "IpProtocol": "tcp",
-                            "FromPort": 22,
-                            "ToPort": 22,
-                            "IpRanges": [{"CidrIp": "0.0.0.0/0"}],
-                        }
-                    ]
-                )
+    security_groups = {
+        "{GroupId} - {GroupName}".format_map(sg): sg["GroupId"]
+        for sg in client.describe_security_groups()["SecurityGroups"]
+    }
+
+    my_ip = requests.get("https://checkip.amazonaws.com").text.strip()
+
+    answers = inquirer.prompt(
+        [
+            inquirer.List(
+                "cidr_ip",
+                "Select which IP address to allow SSH access from",
+                choices=[f"{my_ip}/32", "0.0.0.0/0"],
+            ),
+            inquirer.List(
+                "sg",
+                message="Select a security group to update",
+                choices=list(security_groups.keys()),
+            ),
+        ]
+    )
+
+    sg = answers["sg"]
+    group_id = security_groups[sg]
+    cidr_ip = answers["cidr_ip"]
+
+    print(f"Updating security group: {sg}")
+    with update_permissions(
+        client,
+        group_id,
+        [
+            {
+                "IpProtocol": "tcp",
+                "FromPort": 22,
+                "ToPort": 22,
+                "IpRanges": [{"CidrIp": cidr_ip}],
+            },
+        ],
+    ):
+        try:
+            input("Press any key to continue...")
+        except KeyboardInterrupt:
+            pass
+
+
+@contextmanager
+def update_permissions(client: EC2Client, group_id, ip_permissions):
+    try:
+        try:
+            client.authorize_security_group_ingress(
+                GroupId=group_id,
+                IpPermissions=ip_permissions,
+            )
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "InvalidPermission.Duplicate":
+                print("Ingress rule already exists.")
+            else:
+                raise
+        yield
+    finally:
+        client.revoke_security_group_ingress(
+            GroupId=group_id,
+            IpPermissions=ip_permissions,
+        )
 
 
 if __name__ == "__main__":
